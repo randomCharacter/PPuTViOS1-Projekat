63a80ed508f81ffd55c4c390813995d243417e72
Mario Peric <pericmario96@gmail.com>
2019-03-01 18:11:01 +0100

    Implement mute

--- 5.zapper_app.skeleton/volume_controller.c
+++ 5.zapper_app.skeleton/volume_controller.c
@@ -2,9 +2,11 @@
 
 static uint32_t player = -1;
 static uint16_t volume_level = 0;
+static uint8_t muted = 0;
 
 VolumeControllerError volumeControllerInit(uint32_t playerHandle) {
 	player = playerHandle;
+	muted = 0;
 	if (Player_Volume_Set(player, INIT_VOLUME * VOLUME_LEVEL)) {
 		return VC_ERROR;
 	}
@@ -15,13 +17,13 @@ VolumeControllerError volumeControllerInit(uint32_t playerHandle) {
 }
 
 VolumeControllerError volumeUp() {
-	printf("VoulmeUp()\n");
+	muted = 0;
 	if (volume_level < 10) {
 		volume_level++;
 	} else {
 		volume_level = 10;
 	}
-	if (Player_Volume_Set(player, volume_level * VOLUME_LEVEL)) {
+	if (Player_Volume_Set(player, !muted * volume_level * VOLUME_LEVEL)) {
 		return VC_ERROR;
 	}
 
@@ -33,13 +35,13 @@ VolumeControllerError volumeUp() {
 }
 
 VolumeControllerError volumeDown() {
-	printf("VoulmeDown()\n");
+	muted = 0;
 	if (volume_level > 0) {
 		volume_level--;
 	} else {
 		volume_level = 0;
 	}
-	if (Player_Volume_Set(player, volume_level * VOLUME_LEVEL)) {
+	if (Player_Volume_Set(player, !muted * volume_level * VOLUME_LEVEL)) {
 		return VC_ERROR;
 	}
 
@@ -51,18 +53,19 @@ VolumeControllerError volumeDown() {
 }
 
 VolumeControllerError getVolume(uint16_t *vol) {
-	*vol = volume_level;
+	*vol = volume_level * !muted;
 
 	return VC_NO_ERROR;
 }
 
 VolumeControllerError muteVolume() {
-	volume_level = 0;
-	if (Player_Volume_Set(player, 0)) {
+	muted = !muted;
+
+	if (Player_Volume_Set(player, !muted * volume_level * VOLUME_LEVEL)) {
 		return VC_ERROR;
 	}
 
-	drawVolume(volume_level);
+	drawVolume(!muted * volume_level);
 
 	return VC_NO_ERROR;
 }
